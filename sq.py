d652ad731869877ac1365c953a90c5f7374f158a
maliha-afk <24amaliha@gmail.com>
2024-09-20 18:01:52 +0600

    Add files via upload

---
+++ sq.py
@@ -0,0 +1,12 @@
+number= int(input("enter your number(between 1-100): "))
+
+
+if number%2==0:
+  print(F"the number {number} is a squared number because it's an even number")
+
+elif number%2==1:
+  print(f"the number {number} is not a squared number because it's an odd number")
+  
+else:
+ print("⚠️!ERROR!⚠️")
+ 
